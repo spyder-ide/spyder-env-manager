1822afde052bd2463153e2f680bc000c3262190b
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2025-06-30 16:24:44 -0300

    feat: add api for envs_manager jupyter plugin using remote-client modules api

--- spyder_env_manager/spyder/workers.py
+++ spyder_env_manager/spyder/workers.py
@@ -7,17 +7,30 @@
 # ----------------------------------------------------------------------------
 
 # Standard library imports
+from __future__ import annotations
+import typing as t
 import logging
 
 # Third-party imports
-from envs_manager.api import Manager, ManagerActionResult
+from envs_manager.api import Manager
 from qtpy.QtCore import QObject, Signal
 
 # Spyder and local imports
 from spyder.api.config.mixins import SpyderConfigurationObserver
 from spyder.api.translations import get_translation
-from spyder_env_manager.spyder.api import ManagerRequest
+from spyder.api.asyncdispatcher import AsyncDispatcher
 from spyder_env_manager.spyder.config import CONF_SECTION
+from spyder.plugins.remoteclient.api.modules.base import SpyderBaseJupyterAPI
+from spyder.plugins.remoteclient.api.manager.base import SpyderRemoteAPIManagerBase
+
+if t.TYPE_CHECKING:
+    from aiohttp import ClientResponse
+
+    from envs_manager.api import ManagerActionResult
+
+    from spyder_env_manager.spyder.api import ManagerRequest
+    from spyder_env_manager.spyder.widgets.main_widget import SpyderEnvManagerWidget
+
 
 # Localization
 _ = get_translation("spyder")
@@ -48,7 +61,7 @@ class EnvironmentManagerWorker(QObject, SpyderConfigurationObserver):
         Options of the manager object that is handling the environment.
     """
 
-    def __init__(self, parent, request: ManagerRequest):
+    def __init__(self, parent: SpyderEnvManagerWidget, request: ManagerRequest):
         QObject.__init__(self)
         self._parent = parent
 
@@ -102,3 +115,99 @@ class EnvironmentManagerWorker(QObject, SpyderConfigurationObserver):
             self.sig_ready.emit(status, error_msg or output, manager_options)
         except RuntimeError:
             pass
+
+
+@SpyderRemoteAPIManagerBase.register_api
+class RemoteEnvironmentManagerAPI(SpyderBaseJupyterAPI):
+    """
+    Remote worker to run environment manager actions without blocking the Spyder
+    user interface on a remote server.
+    """
+
+    base_url = "envs_manager"
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the remote environment manager API.
+
+        Parameters
+        ----------
+        args: tuple
+            Positional arguments.
+        kwargs: dict
+            Keyword arguments.
+        """
+        super().__init__(*args, **kwargs)
+        
+        self.error = None
+
+    async def _raise_for_status(self, response: ClientResponse):
+        response.raise_for_status()
+
+    async def run_action(
+        self, request: ManagerRequest
+    ) -> t.Tuple[bool, str, dict]:
+        """
+        Run an environment manager action on the remote server.
+
+        Parameters
+        ----------
+        action: str
+            The action to run.
+        action_options: dict, optional
+            Options for the action.
+
+        Returns
+        -------
+        ManagerActionResult
+            Result of the action.
+        """
+
+        status = False
+        manager_options = {}
+
+        try:
+            result = await self.run_manager_action(request)
+            status = result["status"]
+            output = result["output"]
+            manager_options = result["manager_options"]
+
+            # It's simpler to handle strings than bytes
+            if isinstance(output, bytes):
+                output = output.decode("utf-8")
+
+            # This is necessary because we can't emit a TypedDict in a Qt signal
+            manager_options = dict(manager_options)
+        except Exception as e:
+            error_msg = _(
+                "Unable to run action over environment: "
+                "<br><br> <tt>{exception_string}</tt>"
+            ).format(exception_string=str(e))
+            logger.exception(error_msg)
+            output = error_msg
+            self.error = error_msg
+
+        return status, output, manager_options
+
+    async def run_manager_action(
+        self, request: ManagerRequest
+    ) -> ManagerActionResult:
+        """
+        Run an environment manager action on the remote server.
+
+        Parameters
+        ----------
+        request: ManagerRequest
+            Request containing the action and options.
+
+        Returns
+        -------
+        dict
+            Result of the action.
+        """
+        async with self.session.post(
+            self.api_url / request["action"],
+            json=request["action_options"],
+            params=request["manager_options"]
+            ) as response:
+            return await response.json()
